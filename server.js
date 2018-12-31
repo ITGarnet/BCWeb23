71c7761c01f1e853b98953851c81470b56fd2b6a
Tziki <tzgarnet@gmail.com>
2018-12-31 14:16:11 -0500

    Posting with Postman

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Tziki!");
+/* handling all the parsing */
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+  var email = req.body.email;
+  var amount = req.body.amount;
+
+  res.send({ amount: amount, email: email });
 });
 
 app.listen(port, () => {
