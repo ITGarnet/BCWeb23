3260c39437a56255f0c98aaa25ad19f25afdf598
Tziki <tzgarnet@gmail.com>
2019-01-02 19:10:01 -0500

    Creating Payment with paypal

--- public/index.js
+++ public/index.js
@@ -18,7 +18,7 @@ class App extends React.Component {
       amount: this.state.amount,
       email: this.state.email
     });
-    console.log(response);
+    window.location.href = response.data;
   };
 
   render() {
